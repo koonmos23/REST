23e57970a49c3505e9b35e79447d8cc08a7e04ea
koonmos23 <mosza2304@hotmail.com>
2023-02-09 11:49:06 +0700

    update

--- src/CRUDBookNoDB.js
+++ src/CRUDBookNoDB.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-let book = [
+let books = [
 {
     id: 1,
     title: 'Book 1',
@@ -44,15 +44,15 @@ app.post('/books',(req,res)=>{
 
 app.put('/books/:id',(req,res)=>{
     const book = books.find(b=>b.id === parseInt(req.params.id));
-    if(!book) res.status(404).sent('Book not found');
+    if(!book) res.status(404).send('Book not found');
     book.title = req.body.title;
     book.author = req.body.author;
     res.send(book);
 });
 
-app.delete('/books',(req,res)=>{
+app.delete('/books/:id',(req,res)=>{
     const book = books.find(b=>b.id===parseInt(req.params.id));
-    if(!book) res.status(404).sent('Book not found');
+    if(!book) res.status(404).send('Book not found');
     const index = books.indexOf(book);
     books.splice(index,1);
     res.send(book);
