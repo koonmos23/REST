1dbc336c91bc3578bb1e62171a7cac8444ad624f
koonmos23 <mosza2304@hotmail.com>
2023-02-16 10:57:34 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("HELLO World!");
+  res.send("I'am atomic");
 });
 
 app.listen(port, () => {
